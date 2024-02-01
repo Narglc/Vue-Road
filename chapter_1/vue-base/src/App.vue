887d53fe9071c33dad68275cf563e5014e13200d
Narglc Yu <qingcheliuzhi@qq.com>
2024-02-01 22:35:11 +0800

    文本插值

--- chapter_1/vue-base/src/App.vue
+++ chapter_1/vue-base/src/App.vue
@@ -1,4 +1,19 @@
-<script>
-</script>
 
-<template></template>
+<template>
+  <h3>模版语法</h3>
+  <p>{{ msg }}</p>
+  <p>{{ hello }}</p>
+</template>
+
+
+<script>
+export default {
+  data() {
+    return {
+      msg: "神奇的语法:`文本插值`",
+      hello: "hello world"
+    }
+  }
+}
+
+</script>
