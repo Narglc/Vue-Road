d3c6066556aed916dd7f18fd0305fa63ddb0088d
Narglc Yu <qingcheliuzhi@qq.com>
2024-03-06 23:22:40 +0800

    监听事件：内联事件处理器&方法事件处理器

---
+++ chapter_1/vue-base/src/components/EventFuncDemo.vue
@@ -0,0 +1,23 @@
+<template>
+    <h3>方法事件处理器</h3>
+    <button @dblclick="addCount">Add</button>
+    <p>您点击了 {{ count }} 次! </p>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            count: 0
+        }
+    },
+    // 所有的方法或函数都放在这里
+    methods: {
+        addCount() {
+            // 读取到data里面的数据的方案： this.count
+            this.count += 2
+            console.log("你点击了", this.count);
+        }
+    }
+}
+</script>
---
+++ chapter_1/vue-base/src/components/EventInlineDemo.vue
@@ -0,0 +1,17 @@
+<template>
+    <h3>內联处理器</h3>
+    <p> 内联 工作中很少使用</p>
+    <button v-on:click="count--">Minus</button>
+    <button @click="count++">Add</button>
+    <p>您点击了 {{ count }} 次! </p>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            count: 0
+        }
+    }
+}
+</script>
