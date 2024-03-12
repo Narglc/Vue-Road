cafd2ae810c28828c151452903fe9a3fb92f65b2
Narglc <appleboyharryu@163.com>
2024-03-12 15:49:11 +0800

    组件数据传递: props通过func进行子传父

--- vue-components-event/src/App.vue
+++ vue-components-event/src/App.vue
@@ -1,16 +1,20 @@
 <template>
   <!-- <ComponentEvent /> -->
-  <Main />
+  <!-- <Main /> -->
+  <ComponentA/>
 </template>
 
 <script>
 import ComponentEvent from './components/ComponentEvent.vue';
 import Main from './components/Main.vue'
 
+import ComponentA from './components/ComponentA.vue'
+
 export default {
   components: {
     ComponentEvent,
-    Main
+    Main,
+    ComponentA
   }
 }
 </script>
---
+++ vue-components-event/src/components/ComponentA.vue
@@ -0,0 +1,25 @@
+<template>
+    <h3> ComponentA</h3>
+    <ComponentB :title="title" :onEvent="dataFn"/>
+    <p>子组件传递过来的参数: {{ messageFromSon }}</p>
+</template>
+
+<script>
+import ComponentB from './ComponentB.vue'
+export default{
+    data(){
+        return{
+            title:"标题(传递的)",
+            messageFromSon: ""
+        }
+    },
+    components:{
+        ComponentB
+    },
+    methods:{
+        dataFn(data){
+            console.log(data);
+            this.messageFromSon = data
+        }
+    }
+}</script>
