4213fd9668d5a8a96b8c5c0e5b96827553e25296
Narglc <appleboyharryu@163.com>
2024-03-12 15:56:43 +0800

    透传属性继承, 不经常使用, 仅作了解

--- vue-components-event/src/App.vue
+++ vue-components-event/src/App.vue
@@ -1,7 +1,14 @@
 <template>
   <!-- <ComponentEvent /> -->
   <!-- <Main /> -->
-  <ComponentA/>
+  <!-- <ComponentA/> -->
+
+  <!-- 通过组件方式传递属性, 作用于子组件的根元素
+      使用限制:
+        1. 必须是唯一根元素
+        否则多个根元素时不生效
+    -->
+  <AttrComp class="attr-container" />
 </template>
 
 <script>
@@ -10,11 +17,14 @@ import Main from './components/Main.vue'
 
 import ComponentA from './components/ComponentA.vue'
 
+import AttrComp from './components/AttrComp.vue';
+
 export default {
   components: {
     ComponentEvent,
     Main,
-    ComponentA
+    ComponentA,
+    AttrComp
   }
 }
 </script>
