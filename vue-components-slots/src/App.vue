0617d285b114861b5d61bbc226102ece8229d3aa
Narglc <appleboyharryu@163.com>
2024-03-13 10:20:08 +0800

    插槽动态数据、插槽默认值

--- vue-components-slots/src/App.vue
+++ vue-components-slots/src/App.vue
@@ -1,15 +1,28 @@
 <template>
-  <SlotBase>
+  <!-- <SlotBase>
     <h3> 插槽标题</h3>
     <p> 插槽内容</p>
-  </SlotBase>
+  </SlotBase> -->
+
+  <SlotTwo>
+    <p>1.[插槽动态数据] 插槽中的动态数据要在父组件中定义</p>
+    <h3>{{message}}</h3>
+  </SlotTwo>
 </template>
 
 <script>
 import SlotBase from "./components/SlotBase.vue"
+import SlotTwo  from "./components/SlotTwo.vue"
+
 export default{
+  data(){
+    return{
+      message:"插槽的内容"
+    }
+  },
   components:{
-    SlotBase
+    SlotBase,
+    SlotTwo
   }
 }
 </script>
---
+++ vue-components-slots/src/components/SlotTwo.vue
@@ -0,0 +1,18 @@
+<template>
+    <h3> Slot 续集</h3>
+    <hr>
+    <slot>
+        <p>2.[插槽默认值] 如果父组件没有传递插槽,可使用此部分的默认值进行显示</p>
+    </slot>
+    <hr>
+    <slot></slot>
+</template>
+
+<script>
+export default{
+    data(){
+        return{
+
+        }
+    }
+}</script>
