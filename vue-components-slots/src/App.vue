78001d1053da40ae0d666f6c8863abb79f60c944
Narglc <appleboyharryu@163.com>
2024-03-13 10:27:14 +0800

    具名插槽 v-slot/#, <slot name=>

--- vue-components-slots/src/App.vue
+++ vue-components-slots/src/App.vue
@@ -5,8 +5,13 @@
   </SlotBase> -->
 
   <SlotTwo>
-    <p>1.[插槽动态数据] 插槽中的动态数据要在父组件中定义</p>
-    <h3>{{message}}</h3>
+    <template v-slot:slot1_part>
+      <p>1.[插槽动态数据] 插槽中的动态数据要在父组件中定义</p>
+      <h3>{{message}}</h3>
+    </template>
+    <template #slot2_part>
+      <p> 内容二</p>
+    </template>
   </SlotTwo>
 </template>
 
--- vue-components-slots/src/components/SlotTwo.vue
+++ vue-components-slots/src/components/SlotTwo.vue
@@ -1,11 +1,14 @@
 <template>
     <h3> Slot 续集</h3>
     <hr>
+    <slot name="slot1_part">具名插槽</slot>
+    <hr>
     <slot>
         <p>2.[插槽默认值] 如果父组件没有传递插槽,可使用此部分的默认值进行显示</p>
     </slot>
     <hr>
-    <slot></slot>
+    <slot name="slot2_part"></slot>
+    
 </template>
 
 <script>
